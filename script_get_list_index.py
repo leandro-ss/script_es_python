eed2ca3733308a532a3089c0aa7a66deda287481
leandro-ss <lss.aspira@gmail.com>
2017-07-24 15:01:25 -0300

    code updated

---
+++ script_get_list_index.py
@@ -0,0 +1,12 @@
+#connect to our cluster
+from elasticsearch import Elasticsearch
+es = Elasticsearch([{'host': 'elk_one'}])
+#es = Elasticsearch([{'host': 'elk_two'}],http_auth=('elastic', 'changeme'))
+
+schema = es.indices.get_mapping() ## python dict with the map of the cluster
+
+indices_list = schema.keys()
+just_indices = [index for index in indices_list if not index.startswith(".")] ## remove the objects created by marvel, e.g. ".marvel-date"
+
+import json
+for index in just_indices: print( index)
--- script_get_mapping_es.py
+++ script_get_mapping_es.py
@@ -1,14 +1,9 @@
 #connect to our cluster
 from elasticsearch import Elasticsearch
-es = Elasticsearch([{'host': 'elk_one', 'port': 9200}])
-es = Elasticsearch([{'host': 'elk_two', 'port': 9200}],http_auth=('elastic', 'changeme'))
-
-schema = es.indices.get_mapping() ## python dict with the map of the cluster
-
-indices_list = schema.keys()
-just_indices = [index for index in indices_list if not index.startswith(".")] ## remove the objects created by marvel, e.g. ".marvel-date"
+es = Elasticsearch([{'host': 'elk_one'}])
+#es = Elasticsearch([{'host': 'elk_two'}],http_auth=('elastic', 'changeme'))
 
 import json
-for index in es.indices.get('cloud*'):
+for index in es.indices.get('python-silk-percent'):
     print(json.dumps(es.indices.get_settings(index), sort_keys=True, indent=4))
-    #print(json.dumps(es.indices.get_mapping(index), sort_keys=True, indent=4))
+    print(json.dumps(es.indices.get_mapping(index), sort_keys=True, indent=4))
